9cdddccbb5827a3fb742e8ca283cb85300f0d2b9
apebnb <128845747+musterman7@users.noreply.github.com>
2026-02-03 12:21:47 +0700

    Implement Umami tracking script in Home component
    Add Umami tracking script to the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+import Head from "next/head";
+
 export default function Home() {
-  return null
+  return (
+    <>
+      <Head>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="d5162b74-f7dc-4055-9dda-0754bc2f8a4c"
+        ></script>
+      </Head>
+    </>
+  );
 }
