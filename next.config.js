00f7197e7a97a86b772dc8dbda0871521e4d8419
apebnb <128845747+musterman7@users.noreply.github.com>
2026-02-05 13:40:23 +0700

    Update redirect destination in next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/:path*",
-        destination: "https://four.meme?code=C5UE2M5H89FC",
+        destination: "https://gmgn.ai/r/tFWXJd6e?chain=bsc",
         permanent: false
       }
     ]
