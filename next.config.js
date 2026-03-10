8c3b45a5877be06024f0f4e8e663b4a21b2e9650
apebnb <128845747+musterman7@users.noreply.github.com>
2026-03-10 13:12:10 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/:path*",
-        destination: "https://four.meme/token/0x8d72e879c6274b2af4642ad2e240910de541ffff?code=5J95FU49249Z",
+        destination: "https://four.meme?code=5J95FU49249Z",
         permanent: false
       }
     ]
